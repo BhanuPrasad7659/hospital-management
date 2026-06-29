08ff8d49b15cb5c823707f6fa6aeb5b60b3b93fa
Kamalanathan <2506808@cognizant.com>
2026-06-29 18:25:07 +0530

    Digeesh Updates Branch push

--- obj/Release/net10.0/CogMediHospitalManagementSystem.AssemblyInfo.cs
+++ obj/Release/net10.0/CogMediHospitalManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CogMediHospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8363f526cfd7de65201a976fa6013c718a3058c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CogMediHospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CogMediHospitalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
